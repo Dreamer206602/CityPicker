9ec96c5e95324ba15a1a6e0d225de159ad04ae98
boobooL <boobooMX@163.com>
2016-04-06 10:17:09 +0800

    add content

--- app/src/main/java/com/tq/MainActivity.java
+++ app/src/main/java/com/tq/MainActivity.java
@@ -1,13 +1,32 @@
 package com.tq;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+
+import com.tq.activity.CitySelectActivity;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    @Bind(R.id.btn)
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
+
+    @OnClick(R.id.btn)
+    public void start(){
+        startActivity(new Intent(MainActivity.this, CitySelectActivity.class));
+    }
+
 }
---
+++ app/src/main/java/com/tq/activity/CitySelectActivity.java
@@ -0,0 +1,15 @@
+package com.tq.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.tq.R;
+
+public class CitySelectActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_city_select);
+    }
+}
